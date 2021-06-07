d3286490fe73cd9f14fb6c8bead5238eac770480
quangmanucians <68055977+quangmanucians@users.noreply.github.com>
2021-06-08 03:05:33 +0700

    Footer demo

--- wp-content/themes/my_theme/footer.php
+++ wp-content/themes/my_theme/footer.php
@@ -1,6 +1,151 @@
 <script src="<?php echo get_template_directory_uri() ?>/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+
+<footer id="colophon" class="site-footer">
+    <div class="footer-cms">
+        <div class="theme-container">
+            <div class="service-list service-3">
+                <div class="service-content">
+                    <span class="icon-image"> </span>
+                    <div class="content">
+                        <div class="service_other_text">MONEY BACK GUARATEE! </div>
+                        <div class="service-title">Send within 30 days </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-list service-2">
+                <div class="service-content">
+                    <span class="icon-image"> </span>
+                    <div class="content">
+                        <div class="service_other_text">24/7 CUSTOMER SERVICE </div>
+                        <div class="service-title">Call us 24/7 at 000 - 123 - 456 </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-list service-1">
+                <div class="service-content">
+                    <span class="icon-image"> </span>
+                    <div class="content">
+                        <div class="service_other_text">FREE SHIPPING WORLDWIDE </div>
+                        <div class="service-title">On order over $150 - 7 days a week </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-top">
+        <div class="theme-container">
+            <div id="footer-widget-area">
+                <div id="first" class="first-widget footer-widget">
+                    <aside id="footercontactuswidget-2" class="widget widgets-footercontact">
+                        <h3 class="widget-title">store information</h3>
+                        <ul class="toggle-block">
+                            <li>
+                                <div class="contact_wrapper">
+                                    <div class="address">
+                                        <div class="address_content">
+                                            <div class="contact_address">60, 29th Street, San Francisco, CA 94110, United States of America</div>
+
+                                        </div>
+                                    </div>
+                                    <div class="phone">
+                                        <div class="contact_phone">(+00) 123-456-789 </div>
+
+                                    </div>
+                                    <div class="email">
+                                        <div class="contact_email"><a href="								#" target="_blank">
+                                                demo@example.com</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+                <!-- #first .widget-area -->
+                <div id="second" class="second-widget footer-widget">
+                    <aside id="staticlinkswidget-1" class="widget widgets-static-links">
+                        <h3 class="widget-title">My Account</h3>
+                        <ul class="toggle-block">
+                            <li>
+                                <div class="static-links-list">
+                                    <span><a href="#">
+                                            My orders</a></span>
+
+                                    <span><a href="#">
+                                            My credit slips</a></span>
+
+                                    <span><a href="#">
+                                            My addresses</a></span>
+
+                                    <span><a href="#">
+                                            My personal info</a></span>
+                                    <span><a href="#">
+                                            Privacy Policy</a></span>
+                                </div>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+                <!-- #second .widget-area -->
+                <div id="third" class="third-widget footer-widget">
+                    <aside id="newsletterwidget-1" class="widget widget_newsletterwidget">
+                        <h3 class="widget-title">Subscribe Now</h3><span class="newslettre-text">Contrary to popular belief of lorem Ipsm Latin amet ltin from.</span>
+                        <div class="tnp tnp-widget">
+                            <form method="post" action="http://wordpress.templatemela.com/woo/WCM02/WCM020047/WP3/?na=s" onsubmit="return newsletter_check(this)">
+
+                                <input type="hidden" name="nlang" value="">
+                                <input type="hidden" name="nr" value="widget">
+                                <input type='hidden' name='nl[]' value='0'>
+                                <div class="tnp-field tnp-field-email"><label></label><input class="tnp-email" type="email" name="ne" required></div>
+                                <div class="tnp-field tnp-field-button"><input class="tnp-submit" type="submit" value="Subscribe">
+                                </div>
+                            </form>
+                        </div>
+                    </aside>
+                    <aside id="followmewidget-2" class="widget widgets-follow-us">
+                        <h3 class="widget-title">Follow Us</h3>
+                        <div id="follow_us" class="follow-us">
+                            <ul class="toggle-block">
+                                <li>
+                                    <a href="#" title="Facebook" class="facebook icon"><i class="fa fa-facebook"></i></a>
+                                    <a href="#" title="Twitter" class="twitter icon"><i class="fa fa-twitter"></i></a>
+                                    <a href="#" title="Linkedin" class="linkedin icon"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#" title="RSS" class="rss icon"><i class="fa fa-rss"></i></a>
+                                    <a href="#" title="Google Plus " class="google-plus icon"><i class="fa fa-google-plus"></i></a>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </aside>
+                </div>
+                <!-- #third .widget-area -->
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="theme-container">
+            <div class="footer-bottom-container">
+                <div class="footer-bottom-left">
+                    <div class="site-info"> Copyright &copy; 2021 <a href="javascript:if(confirm(%27https://www.google.co.in/  \n\nThis file was not retrieved by Teleport Ultra, because it is addressed on a domain or path outside the boundaries set for its Starting Address.  \n\nDo you want to open it from the server?%27))window.location=%27https://www.google.co.in/%27" tppabs="https://www.google.co.in/" title="Hifidresy_bag" rel="home">Templatemela. </a>
+                    </div>
+                </div>
+                <aside id="accepted_payment_methods-2" class="widget widget_accepted_payment_methods">
+                    <h3 class="widget-title">Payment</h3>
+                    <ul class="accepted-payment-methods">
+                        <li class="maestro"><span>Maestro</span></li>
+                        <li class="mastercard"><span>MasterCard</span></li>
+                        <li class="paypal"><span>PayPal</span></li>
+                        <li class="visa"><span>Visa</span></li>
+                    </ul>
+                </aside>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
