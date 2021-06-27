92ac0bfd3f5bdcf109e0950787238ac0d86108d2
shidokun557 <67572721+shidokun557@users.noreply.github.com>
2021-06-27 11:55:32 +0700

    con css

--- wp-content/themes/my_theme/header.php
+++ wp-content/themes/my_theme/header.php
@@ -8,7 +8,236 @@
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    <?php wp_head(); ?>
+    <link rel="stylesheet" href="header.css">
+
+        <!-- Welcome to Hifidresy_bag -->
+
+        <div class="topbar-outer">
+    	<div class="topbar-main theme-container">			
+
+		<span class="site-welcome-message"
+        ><span>Welcome to&nbsp;Hifidresy_bag</span>
+        </span>
+										
+    <div class="top_right">
+    <!--  top right -->
+
+        <!-- search -->
+        <div class="header-search">
+
+        
+
+        <div class="header-toggle"></div>
+
+        <form role="search" method="get" class="product-search" action="#">
+            <label class="screen-reader-text" for="product-search-field-0">Search for:</label>
+	            <input type="search" id="product-search-field-0" class="search-field" placeholder="Search products…" value="" name="s">
+	                <button type="submit" value="Search">Search</button>
+	            <input type="hidden" name="post_type" value="product">
+        </form>
+        </div>
+
+        <!-- cart -->
+
+        <!-- <div class="header-cart headercart-block">
+						<div class="cart togg">
+		
+			<div class="shopping_cart tog" title="View your shopping cart">
+					<a class="cart-contents" href="#" title="View your shopping cart">0</a>
+
+							</div>	
+			<aside id="widget_cart-2" class="shopping_cart tab_content"><h3 class="widget-title">Cart</h3>
+
+            <div class="widget_shopping_cart_content">
+
+	    <p class="mini-cart__empty-message">No products in the cart.</p>
+
+            </div>
+            </aside>		
+								</div>							
+									</div> -->
+
+					</div><!--  top right -->
+                    </div>
+   		</div>
+
+           <!-- header-main -->
+
+           <div class="site-header-main">
+<div class="header-main  header-fix">
+<div class="theme-container">	
+		<!-- Start header_left -->	
+
+		<div class="header_left">
+		
+		<!-- #site-navigation -->
+	<nav id="site-navigation" class="navigation-bar main-navigation more">																			
+		<h3 class="menu-toggle">Menu</h3>
+			<a class="screen-reader-text skip-link" href="#" title="Skip to content">Skip to content</a>	
+				<div class="mega-menu">
+					<div class="menu-mainmenu-container">
+                        <ul id="menu-mainmenu" class="mega">
+                            <li id="menu-item-home" class="menu-item menu-home">
+                                <a href="#" aria-current="page">Home</a></li>
+
+          <!-- shop -->
+                                <li id="menu-item-shop" class="menu-item shop"><a href="#" >Shop<span class="dc-mega-icon"></span></a>
+<!-- css -->
+                                <div class="sub-container mega" style="left: 0px; top: 63px; z-index: 1000; display:none"><ul class="sub-menu sub">
+
+	                            <div class="row" style="width: 462px;">
+
+        <!-- shop layout -->
+                                <li id="menu-item-shop-layout" class="menu-item shop-layout"><a href="#" class="mega-hdr-a" style="height: 24px;">Shop Layout</a>
+	                                <ul class="sub-menu">
+		                            <li id="menu-item-left" class="menu-item left"><a href="#">Left Sidebar</a></li>
+		                            <li id="menu-item-right" class="menu-item right"><a href="#">Right Sidebar</a></li>
+		                            <li id="menu-item-full-width" class="menu-item full-width"><a href="#">Full Width</a></li>
+		                            <li id="menu-item-product-brand" class="menu-item product-brand"><a href="#">Products Brands</a></li>
+		                            <li id="menu-item-product-category" class="menu-item product-category"><a href="#">Product Category</a></li>
+	                     </ul>
+
+
+                                </li>
+
+        <!-- product-type(shop) -->
+
+    <li id="menu-item-product-type" class="menu-item product-type"><a href="#" class="mega-hdr-a" style="height: 24px;">Product Type</a>
+	    <ul class="sub-menu">
+	        <li id="menu-item-variable-product" class="menu-item variable-product"><a href="#">Variable Product</a></li>
+		    <li id="menu-item-external-product" class="menu-item external-product"><a href="#">External Product</a></li>
+		    <li id="menu-item-group-product" class="menu-item group-product"><a href="#">Group Product</a></li>
+		    <li id="menu-item-special-product" class="sale-label special_product"><a href="#">Special Products</a></li>
+		    <li id="menu-item-rated-product" class="menu-item rated-product"><a href="#">Top rated product</a></li>
+	    </ul>
+    </li>
+</div>
+	
+    </ul>
+    </div>
+
+</li>
+
+    <!-- blog -->
+<li id="menu-item-blog" class="menu-item blog"><a href="#" class="dc-mega">Blog<span class="dc-mega-icon"></span></a>
+<!-- css -->
+<div class="sub-container non-mega" style="left: 147.031px; top: 63px; z-index: 1000; display:none"><ul class="sub-menu sub">
+	
+<li id="menu-item-left-slidebar" class="menu-item left-slidebar"><a href="#">Left Sidebar</a></li>
+	<li id="menu-item-right-slidebar" class="menu-item right-slidebar"><a href="#">Right Sidebar</a></li>
+	<li id="menu-item-full-width" class="menu-item full-width"><a href="#">Full Width</a></li>
+</ul></div>
+</li>
+    
+<!-- more -->
+
+<li class="cat-item level-0 hiden_menu cat-parent dc-mega-li"><a href="#" class="level-0  activSub dc-mega">More<span class="dc-mega-icon"></span></a>
+
+<div class="sub-container mega" style="left: 0px; top: 63px; z-index: 1000;">
+<ul class="children sub" style="display: none" ><div class="row" style="width: 693px;">
+<li id="menu-item-6516" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-6516 mega-unit mega-hdr" style="">
+
+<a href="#" class="mega-hdr-a" style="height: 24px;">Media</a>
+
+<ul class="sub-menu">
+	<li id="menu-item-gallery" class="menu-item gallery"><a href="#">Gallery</a>
+	<ul class="sub-menu">
+		<li id="menu-item-2-columns" class="menu-item 2-columns"><a href="#">2 Columns</a></li>
+		<li id="menu-item-3-columns" class="menu-item 3-columns"><a href="#">3 Columns</a></li>
+		<li id="menu-item-4-columns" class="menu-item 4-columns"><a href="#">4 Columns</a></li>
+	</ul>
+</li>
+
+<li id="menu-item-portfolio" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-portfolio"><a href="#">Portfolio</a>
+	<ul class="sub-menu">
+		<li id="menu-item-2-columns" class="menu-item 2-columns"><a href="#">2 Columns</a></li>
+		<li id="menu-item-3-columns" class="menu-item 3-columns"><a href="#">3 Columns</a></li>
+		<li id="menu-item-4-columns" class="menu-item 4-columns"><a href="#">4 Columns</a></li>
+	</ul>
+</li>
+</ul>
+</li>
+
+<li id="menu-item-shortcode" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-6491 mega-unit mega-hdr" style="height: 430px;">
+<a href="#" class="mega-hdr-a" style="height: 24px;">ShortCode</a>
+<ul class="sub-menu">
+	<li id="menu-item-shortcode-page" class="menu-item shortcode-page"><a href="#">Shortcode Pages</a>
+	<ul class="sub-menu">
+		<li id="menu-item-accordion" class="menu-item accordion"><a href="#">Accordions &amp; Toggles</a></li>
+		<li id="menu-item-buttons" class="menu-item buttons"><a href="#">Buttons</a></li>
+		<li id="menu-item-drivider" class="menu-item drivider"><a href="#">Divider</a></li>
+		<li id="menu-item-bar" class="menu-item bar"><a href="#">Progress Bar &amp;  Pie Chart</a></li>
+		<li id="menu-item-lists" class="menu-item lists"><a href="#">Lists</a></li>
+		<li id="menu-item-service" class="menu-item service"><a href="#">Services</a></li>
+		<li id="menu-item-tab" class="menu-item tab"><a href="#">Tabs</a></li>
+		<li id="menu-item-map-contact" class="menu-item map-contact"><a href="#">Map &amp; Contact</a></li>
+		<li id="menu-item-message-box" class="menu-item message-box "><a href="#">Message Boxes</a></li>
+		<li id="menu-item-pricing-table" class="menu-item pricing-table"><a href="#">Pricing table</a></li>
+	</ul>
+</li>
+</ul>
+</li>
+
+<li id="menu-item-features" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-features mega-unit mega-hdr last" style="height: 430px;">
+
+<a href="#" class="mega-hdr-a" style="height: 24px;">Features</a>
+
+<ul class="sub-menu">
+	<li id="menu-item-faqs-page" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-faqs-page"><a href="#">FAQs Page</a></li>
+	<li id="menu-item-typography" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-typography"><a href="#">Typography</a></li>
+	<li id="menu-item-sitemap" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-sitemap"><a href="#">Sitemap</a></li>
+	<li id="menu-item-contact" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-contact"><a href="#">Contact Us</a></li>
+	<li id="menu-item-about" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-about"><a href="#">About Us</a></li>
+</ul>
+</li></div></ul></div></li>
+
+
+</ul></div>	
+									 			
+								</div>	
+
+                                <div class="header_center">
+							<!-- Header LOGO-->
+							<div class="header-logo">
+									<a href="#" title="Hifidresy_bag" rel="home">
+								<img alt="Hifidresy" src="./img/Mob-logo.png"></a>
+							</div>
+						</div>	
+
+                        <!-- Start header_right -->	
+						<div class="header_right">
+			
+            <!-- Header topbar menu -->
+                <div class="topbar-links">
+                    <div class="topbar-main">													
+                            
+                            
+                            <!-- Header Menu Start  -->
+                                
+        
+                                <div class="header-menu-links">		
+                            <span class="top-menu-toggle"></span>		
+
+<ul id="menu-header-top-links" class="header-menu"><li id="menu-item-about-us" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-about-us">
+<a href="#">About Us</a></li>
+<li id="menu-item-account" class="account menu-item menu-item-type-post_type menu-item-object-page menu-item-account"><a href="#">My Account</a></li>
+<li id="menu-item-wishlist" class="wishlist menu-item menu-item-type-post_type menu-item-object-page menu-item-wishlist"><a href="#">Wishlist</a></li>
+</ul>									</div>
+                                 
+                            <!-- Header Menu End  -->			
+                            
+                </div>
+            </div>
+
+            </div>			    
+  <!-- End header-main -->
+</div>	
+
 </head>
+
+
+
+
+
 
 <body>
