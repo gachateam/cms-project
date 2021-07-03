bbb689810abf7b05ad1e5a93c2bf310102535f55
baobaote00 <baobaote00@gmail.com>
2021-07-03 21:03:09 +0700

    :heart: add breadcrumbs

--- wp-content/themes/my_theme/inc/mytheme-funtion.php
+++ wp-content/themes/my_theme/inc/mytheme-funtion.php
@@ -60,7 +60,10 @@ if (!function_exists('storefront_page_header')) {
                 the_post_thumbnail('full');
             }
             the_title('<h1 class="entry-title">', '</h1>');
+            the_breadcrumb();
             ?>
+
+
         </header><!-- .entry-header -->
     <?php
     }
@@ -87,5 +90,56 @@ if (!function_exists('storefront_page_content')) {
             ?>
         </div><!-- .entry-content -->
 <?php
+    }
+}
+
+if (!function_exists("the_breadcrumb")) {
+    function the_breadcrumb()
+    {
+        echo '<ul id="crumbs">';
+        if (!is_home()) {
+            echo '<li><a href="';
+            echo get_option('home');
+            echo '">';
+            echo 'Home';
+            echo "</a></li>&nbsp;/&nbsp;";
+            if (is_category() || is_single()) {
+                echo '<li>';
+                the_category(' </li>&nbsp;/&nbsp;<li> ');
+                if (is_single()) {
+                    echo "</li>&nbsp;/&nbsp;<li>";
+                    the_title();
+                    echo '</li>';
+                }
+            } elseif (is_page()) {
+                echo '<li>';
+                echo the_title();
+                echo '</li>';
+            }
+        } elseif (is_tag()) {
+            single_tag_title();
+        } elseif (is_day()) {
+            echo "<li>Archive for ";
+            the_time('F jS, Y');
+            echo '</li>';
+        } elseif (is_month()) {
+            echo "<li>Archive for ";
+            the_time('F, Y');
+            echo '</li>';
+        } elseif (is_year()) {
+            echo "<li>Archive for ";
+            the_time('Y');
+            echo '</li>';
+        } elseif (is_author()) {
+            echo "<li>Author Archive";
+            echo '</li>';
+        } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {
+            echo "<li>Blog Archives";
+            echo '</li>';
+        } elseif (is_search()) {
+            echo "<li>Search Results";
+            echo '</li>';
+        }
+        echo '</ul>';
     }
 }
